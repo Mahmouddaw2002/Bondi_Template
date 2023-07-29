1f57436ae1ad83ab62a64006ff1f5add3fc712f9
Mahmouddaw2002 <mahmouddaw1347@gmail.com>
2023-07-29 21:41:47 +0300

    add language

---
+++ js/script.js
@@ -0,0 +1,165 @@
+const translations = {
+  en: {
+    Home: "Home",
+    Services: "Services",
+    Portfolio: "Portfolio",
+    About: "About",
+    Contact: "Contact",
+    Login: "Login",
+    desc1: "We make awesome graphic and web design",
+    desc2: " Some Of These Stuff Under",
+    desc3:
+      "Pellentesque in ipsum id orci porta dapibus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.",
+    desc4: "Prepare To Be Amazed",
+    desc5: "The Great Team",
+    desc6:
+      "Donec rutrum congue leo eget malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus. Donec sollicitudin molestie malesuada.",
+    desc7:
+      " Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.",
+    desc8:
+      " Donec rutrum congue leo eget malesuada. Mauris blandit aliquetelit, eget tincidunt nibh pulvinar a. Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus. Donec sollicitudinmolestie malesuada.",
+    desc9:
+      " Donec rutrum congue leo eget malesuada. Mauris blandit aliquet elit,eget tincidunt nibh pulvinar a. Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus. Donec sollicitudin molestie malesuada.",
+    desc10:
+      "“I don't understand how we got by those troops. I thought we were dead.“",
+    desc11:
+      " Leave your description and we start the engine.Don't worry,you can cancel anytime",
+    desc12: "New Stories",
+    desc13: " Some Awesome Blog Title Here",
+    desc14:
+      "Pellentesque in ipsum id orci porta dapibus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.",
+    desc15: "Create By Graphberry",
+    desc16: "Bondi Inc",
+    desc17:
+      "   Get in touch with us via mail phone.We are waiting for your call or message",
+    header1: "Taste the Creativity",
+    header2: "We are Good at",
+    header3: "We Make This",
+    header4: "Some Stuff About Us",
+    header5: "Retina Design",
+    header6: "Meet The Team",
+    header7: "Start Your Project Now",
+    header8: "Read Our Blog",
+    header9: "Subscribe to our Newsletter:",
+    header10: "Links",
+    header11: "About Us",
+    header12: "Contact Us",
+    Get_Started: "Get Started",
+    Graphic_Design: "Graphic Design",
+    All: "All",
+    Design: "Design",
+    Code: "Code",
+    Photo: "Photo",
+    App: "App",
+    btn_More: "I Want More",
+    btn_Order: "Order Me One",
+    name_Card: "Luke Skywalker",
+    btn_start: "Start Project",
+    btn_Stories: "More Stories",
+    li_home: "Home",
+    li_services: "Our Services",
+    li_portfolio: "Portfolio",
+    li_testimonials: "Testimonials",
+    li_support: "Support",
+    li_terms: "Terms and Condition",
+    li_Sign: "Sign In",
+    li_Register: "Register",
+    li_About: "About Us",
+    li_Blog: "Blog",
+    li_Contact: "Contact Us",
+    lang: "Lang",
+  },
+  ar: {
+    Home: "الصفحة الرئيسية",
+    Services: "الخدمات",
+    Portfolio: "مَلَفّ",
+    About: "عنا",
+    Contact: "اتصال بنا",
+    Login: "تسجيل الدخول",
+    desc1: "نصنع تصميم رسومات ومواقع رهيبة",
+    desc2: " بعض هذه الأشياء تحت",
+    desc3:
+      "التغذية في شبكة هذه البوابة السريرية للبروتين. Live Lough Just ، تنورة تحتاج إلى تعزيز ولكن ، في المرح.",
+    desc4: "استعد لتندهش",
+    desc5: "الفريق العظيم",
+    desc6:
+      "حتى ذلك الحين ، علي أن أقوم بواجبي المنزلي. موريس يغري النخبة ، يحتاج إلى tincidunt nibh pulvinar a. يدخل الأطفال من باب العيادة. يحتاج إلى ابتسامة كبيرة. حتى قلق الموظف كان متوقعا.",
+    desc7:
+      "يعتبر المبنى والساحة أكثر أهمية من عنصر السيارة ولكنه مهم للمالك. موريس يغري النخبة ، يحتاج إلى tincidunt nibh pulvinar a.",
+    desc8:
+      " يعتبر المبنى والساحة أكثر أهمية من عنصر السيارة ولكنه مهم للمالك. موريس يغري النخبة ، يحتاج إلى tincidunt nibh pulvinar a.",
+    desc9:
+      "يعتبر المبنى والساحة أكثر أهمية من عنصر السيارة ولكنه مهم للمالك. موريس يغري النخبة ، يحتاج إلى tincidunt nibh pulvinar a.",
+    desc10: "“لا أفهم كيف حصلنا على تلك القوات. اعتقدت أننا ماتنا.“",
+    desc11: " اترك وصفك ونبدأ المحرك. لا تقلق ، يمكنك الإلغاء في أي وقت",
+    desc12: "قصص جديدة",
+    desc13: " بعض عنوان المدونة الرائع هنا",
+    desc14:
+      "التغذية في شبكة هذه البوابة السريرية للبروتين. Live Lough Just ، تنورة تحتاج إلى تعزيز ولكن ، في المرح.",
+    desc15: "إنشاء بواسطة Grapherry",
+    desc16: "Bondi Inc",
+    desc17: "   تواصل معنا عبر هاتف البريد. نحن ننتظر مكالمتك أو رسالتك",
+    header1: "تذوق الإبداع",
+    header2: "نحن جيدون في",
+    header3: "نجعل هذا",
+    header4: "نجعل هذا",
+    header5: "تصميم شبكية العين",
+    header6: "تصميم شبكية العين",
+    header7: "ابدأ مشروعك الآن",
+    header8: "ابدأ مشروعك الآن",
+    header9: "اشترك في نشرتنا الإخبارية:",
+    header10: "الروابط",
+    header11: "معلومات عنا",
+    header12: "اتصل بنا",
+    Get_Started: "البدء",
+    Graphic_Design: "التصميم الجرافيكي",
+    All: "الكل ",
+    Design: "تصميم",
+    Code: "كود",
+    Photo: "صور",
+    App: "برنامج",
+    btn_More: "انا اريد اكثر",
+    btn_Order: "أمرني واحد",
+    name_Card: "لوك سكاي ووكر",
+    btn_start: "ابدأ المشروع",
+    btn_Stories: "المزيد من القصص",
+    li_home: "الرئيسية",
+    li_services: "حدماتنا",
+    li_portfolio: "مَلَفّ",
+    li_testimonials: "الشهادات - التوصيات",
+    li_support: "الدعم",
+    li_terms: "أحكام وشروط",
+    li_Sign: "تسجيل الدخول",
+    li_Register: "يسجل",
+    li_About: "معلومات عنا",
+    li_Blog: "Blog",
+    li_Contact: "اتصل بنا",
+    lang: "اللغة",
+  },
+};
+
+const languageSelector = document.querySelector("#lang");
+// const langButton = document.querySelector("#lang");
+
+languageSelector.addEventListener("click", (event) => {
+  const currentLanguage = languageSelector.textContent.trim();
+  const newLanguage = currentLanguage === translations.en.lang ? "ar" : "en";
+  setLanguage(newLanguage);
+  languageSelector.textContent = translations[newLanguage].lang;
+  localStorage.setItem("lang", newLanguage);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const language = localStorage.getItem("lang") || "en";
+  languageSelector.textContent = translations[language].lang;
+  setLanguage(language);
+});
+
+const setLanguage = (language) => {
+  const elements = document.querySelectorAll("[data-i18n]");
+  elements.forEach((element) => {
+    const translationKey = element.getAttribute("data-i18n");
+    element.textContent = translations[language][translationKey];
+  });
+  document.dir = language === "ar" ? "rtl" : "ltr";
+};
